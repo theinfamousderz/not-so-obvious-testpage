272ff7601211146d7502e5ed0a752b51adc74c2f
theinfamousderz <theinfamousderz@outlook.com>
2016-12-17 22:40:38 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
+//thanks to https://www.youtube.com/watch?v=KWoJgHFYWxY
+
+var n = 0;
+var c = 3;
+var colour = 0;
+var colourSlider;
 function setup() {
-	createCanvas(800, 800);
+	createCanvas(600, 600);
+	background(25);
+	angleMode(DEGREES);
+	colorMode(HSB);
+	colourSlider = createSlider(50, 400, 120);
+	colourSlider.position(20, 20);
+	frameRate(60);
 }
 
 function draw() {
-  background(25);
-  fill(255, 0 ,0);
+	
+	colour = colourSlider.value();
+
+	var a = n * 137.5;
+	var r = c * sqrt(n);
+
+	var x = r * cos(a) + width/2;
+	var y = r * sin(a) + height/2;
+	noStroke();
+	fill(colour, colour, colour);
+	ellipse(x, y, 4 ,4);
+	n++;
 }
+
